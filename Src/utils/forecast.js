950f751d490f9b598f55f8b7b5739170a9670a45
Piyush Singhal <singhalpiyush21@gmail.com>
2021-03-18 20:16:55 +0530

    Initial Commit

---
+++ Src/utils/forecast.js
@@ -0,0 +1,21 @@
+const { response } = require('express')
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherapi.com/v1/current.json?key=774ce0f814fa40ee856165601211003&q=' + latitude + ',' + longitude
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined)
+        }
+        else if (response.body.error) {
+            callback('Unable to find location', undefined)
+        }
+        else {
+            data = 'The Temperature currently out there in '+response.body.location.name+' is '+response.body.current.temp_c + ' degrees. ' + response.body.current.condition.text+' conditions.'
+            callback(undefined, data)
+        }
+    })
+}
+
+module.exports = forecast
